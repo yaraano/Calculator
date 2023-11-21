ea8ed89f156b7af16f278248c2257334b021dabf
yaraano <qwertystar24@gmail.com>
2023-11-21 10:17:12 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        let [state, setState] = useState('0');
+
+        const handleNum = (e) => {
+            const numValue = (e.target.innerText);
+            if (state === '0') {
+                setState(numValue)
+            } else {
+                setState((prev) => prev + numValue)
+            }
+        }
+    const handleNull = (e) => {
+        const numValue = (e.target.innerText);
+        if (state !== '0') {
+            setState((prev) => prev + numValue);
+        }
+    }
+
+        const handleClear = () => {
+            setState(state = '0')
+        }
+        const handleDelete = () => {
+            if (state !== '0') {
+                setState(state.slice(0, -1))
+            }
+        }
+        const handlePlus = (e) => {
+            const numValue = (e.target.innerText)
+            if (!state.includes("+")) {
+                setState((prev) => prev + numValue);
+            }
+        }
+    const handleMinus = (e) => {
+        const numValue = (e.target.innerText)
+        if (!state.includes("-")) {
+            setState((prev) => prev + numValue);
+        }
+    }
+        const handleSum = (e) => {
+            setState(state = eval(state))
+        }
+
+    return (
+        <div className={"container"}>
+            <h3>{state}</h3>
+            <div className="box">
+                <h4 onClick={handleNum}>1</h4>
+                <h4 onClick={handleNum}>2</h4>
+                <h4 onClick={handleNum}>3</h4>
+                <h4 onClick={handleNum}>4</h4>
+                <h4 onClick={handleNum}>5</h4>
+                <h4 onClick={handleNum}>6</h4>
+                <h4 onClick={handleNum}>7</h4>
+                <h4 onClick={handleNum}>8</h4>
+                <h4 onClick={handleNum}>9</h4>
+                <h4 onClick={handleMinus}>-</h4>
+                <h4 onClick={handleNull}>0</h4>
+                <h4 onClick={handlePlus}>+</h4>
+                <h6 onClick={handleClear} >Clear</h6>
+                <h6 onClick={handleDelete} >Delete</h6>
+                <h4 onClick={handleSum}>=</h4>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
